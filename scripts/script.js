ee7dfa15323079dbc0652a8e2f7e4650566be031
annie <annie_marie@estudante.sesisenai.org.br>
2026-04-08 08:46:38 -0300

    add 7 - lets

--- scripts/script.js
+++ scripts/script.js
@@ -3,21 +3,47 @@ document.getElementById("formAdocao").addEventListener("submit", function (e) {
 
     let nome = document.getElementById("nome").value;
     let email = document.getElementById("email").value;
-    let telefone = document.getElementById("telefone").value
-    let cpf = document.getElementById("cpf").value
-    let idade = document.getElementById("idade").value
-    let cidade = document.getElementById("cidade").value
+    let telefone = document.getElementById("telefone").value;
+    let cpf = document.getElementById("cpf").value;
+    let idade = document.getElementById("idade").value;
+    let cidade = document.getElementById("cidade").value;
     let moradia = document.getElementById("moradia").value;
-    let quintal = document. querySelector(' input [ name="quintal" ] : checked' );
-    let antes = document. querySelector(' input [ name="antes" ] : checked' );
-    let 
-
-
+    let quintal = document.querySelector('input[name="quintal"]:checked');
+    let antes = document.querySelector('input[name="antes"]:checked');
+    let dia = document.getElementById("dia").value;
+    let hora = document.getElementById("horas").value;
+    let motivo = document.getElementById("motivo").value;
+    let responsabilidade = document.getElementById("responsabilidade").checked;
 
     if (nome.length < 3) return alert("Nome Inválido");
+    
+    if (telefone.length < 8) return alert("Telefone inválido");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!<br>" + "Nome" +
         nome;
-
+    
+   
 
 });
