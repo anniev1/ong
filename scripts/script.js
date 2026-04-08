f04a598570186fb424ed8fb228208a28027edf15
annie <annie_marie@estudante.sesisenai.org.br>
2026-04-08 08:03:17 -0300

    add - 5 arer

--- scripts/script.js
+++ scripts/script.js
@@ -5,9 +5,15 @@ document.getElementById("formAdocao").addEventListener("submit", function (e) {
     let email = document.getElementById("email").value;
     let telefone = document.getElementById("telefone").value
     let cpf = document.getElementById("cpf").value
+    let idade = document.getElementById("idade").value
+    let cidade = document.getElementById("cidade").value
     let moradia = document.getElementById("moradia").value;
-    let quintal = document.querySelector('input["name="quintal"]:checked');
-    let
+    let quintal = document. querySelector(' input [ name="quintal" ] : checked' );
+    let antes = document. querySelector(' input [ name="antes" ] : checked' );
+    let 
+
+
+
     if (nome.length < 3) return alert("Nome Inválido");
 
     document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!<br>" + "Nome" +
