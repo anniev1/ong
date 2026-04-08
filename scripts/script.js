5c866bb11da40696261a2e660b8839f5c5f67458
annie <annie_marie@estudante.sesisenai.org.br>
2026-04-08 11:29:24 -0300

    add -10 ajuste no html e java

--- scripts/script.js
+++ scripts/script.js
@@ -15,17 +15,17 @@ document.getElementById("formAdocao").addEventListener("submit", function (e) {
     let responsabilidade = document.getElementById("responsabilidade").checked;
 
     if (nome.length < 3) return alert("Nome Inválido");
-    if (email.includes('@')) return alert("email Inválido");
+    if (!email.includes('@')) return alert("Email Inválido")
     if (telefone.length < 8) return alert ("telefone Inválido");
     if (cpf === "")return alert ("cpf Inválido");
-    if (idade >= 18)return alert ("idade incorreta!")
+    if (isNaN(idade) || idade < 18)return alert ("idade incorreta!")
     if (cidade === "") return alert ("Por favor, selecione uma cidade")
     if (moradia === "") return alert ("obrigatorio!")
-    if (quintal === "") return alert ("Obrigatorio!")
-    if (antes === "") return alert ("Obrigatorio!")
-    if (hora < 0 || hrs > 23 || min < 0 || min > 59) return alert ("Retorna o valor numérico válido")
-    if (motivo.trim().length < 10) return alert("O motivo deve ter pelo menos 10 caracteres.");
-    if (!responsabilidade.checked) { alert('Você deve aceitar os termos e condições para continuar.')};
+    if (!quintal) return alert ("Obrigatorio!")
+    if (!antes) return alert ("Obrigatorio!")
+    if (hora === "" || hora < 0) return alert("Informe um número de horas válido");
+    if (motivo.length < 10) return alert("O motivo deve ter pelo menos 10 caracteres.");
+    if (!responsabilidade) { return alert('Você deve aceitar os termos e condições para continuar.');
 
 
 
